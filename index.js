52131c62d8eca99e8f0ebe15da3d493294bd816c
Yash Midha <72779011+YashMidha@users.noreply.github.com>
2025-08-29 11:04:02 +0530

    Post route - bfhl

---
+++ index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = 3000;
+
+const FULL_NAME = "yash_midha"; 
+const DOB = "14062003"; 
+const EMAIL = "yashmidha14715@gmail.com";
+const REG_NUMBER = "22BDS0440";
+
+app.post("/bfhl", (req, res) => {
+  try {
+    const data = req.body.data || [];
+
+    let odd = [];
+    let even = [];
+    let alphabets = [];
+    let specials = [];
+    let sum = 0;
+
+    data.forEach(item => {
+      if (!isNaN(item)) {
+        let num = parseInt(item);
+
+        if (num % 2 === 0) {
+          even.push(item);
+        } else {
+          odd.push(item);
+        }
+        sum += num;
+      } 
+      
+      else if (/^[a-zA-Z]+$/.test(item)) {
+        alphabets.push(item.toUpperCase());
+      } 
+
+      else {
+        specials.push(item);
+      }
+    });
+
+    console.log(alphabets);
+    let concat = alphabets.join("");
+    let reversed = concat.split("").reverse();
+    let altCaps = reversed.map((char, i) =>
+      i % 2 === 0 ? char.toUpperCase() : char.toLowerCase()
+    ).join("");
+
+    res.status(200).json({
+      is_success: true,
+      user_id: `${FULL_NAME}_${DOB}`,
+      email: EMAIL,
+      roll_number: REG_NUMBER,
+      odd_numbers: odd,
+      even_numbers: even,
+      alphabets: alphabets,
+      special_characters: specials,
+      sum: sum.toString(),
+      concat_string: altCaps
+    });
+
+  } 
+  catch (err) {
+    res.status(500).json({ is_success: false, message: err.message });
+  }
+});
+
+app.listen(PORT, () =>  { 
+    console.log(`Server running on ${PORT}`)
+});
